982a27d63f7d8b36e1f3ed5318d67845f7da2e85
suhongkim98 <kshz9815@naver.com>
2021-09-19 15:23:51 +0900

    spring 기본 익셉션 핸들링

--- src/main/java/com/laika/IoT/exception/GlobalExceptionHandler.java
+++ src/main/java/com/laika/IoT/exception/GlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package com.laika.IoT.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,5 +25,39 @@ public class GlobalExceptionHandler {
                 .build();
         return new ResponseEntity<>(response, ErrorCode.REQUEST_PARAMETER_BIND_FAILED.getStatus());
     }
+    @ExceptionHandler(NoHandlerFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        // 없는 경로로 요청 시
 
+        ErrorResponse response = ErrorResponse.builder()
+                .message(ErrorCode.NOT_FOUND_PATH.getMessage())
+                .status(ErrorCode.NOT_FOUND_PATH.getStatus().value())
+                .code(ErrorCode.NOT_FOUND_PATH.getCode())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.NOT_FOUND_PATH.getStatus());
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
+        // GET POST 방식이 잘못된 경우
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message(ErrorCode.METHOD_NOT_ALLOWED.getMessage())
+                .status(ErrorCode.METHOD_NOT_ALLOWED.getStatus().value())
+                .code(ErrorCode.METHOD_NOT_ALLOWED.getCode())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.METHOD_NOT_ALLOWED.getStatus());
+    }
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+
+        ErrorResponse response = ErrorResponse.builder()
+                .message(ErrorCode.UNSUPPORTED_MEDIA_TYPE.getMessage())
+                .status(ErrorCode.UNSUPPORTED_MEDIA_TYPE.getStatus().value())
+                .code(ErrorCode.UNSUPPORTED_MEDIA_TYPE.getCode())
+                .build();
+
+        return new ResponseEntity<>(response, ErrorCode.UNSUPPORTED_MEDIA_TYPE.getStatus());
+    }
 }
