76989c41e63f05c1b36976a927c4cfb0977de202
suhongkim98 <kshz9815@naver.com>
2021-09-19 15:21:20 +0900

    global exception handler 구현 및 validation exception 핸들링

---
+++ src/main/java/com/laika/IoT/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.laika.IoT.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "AUTH_001", " AUTHENTICATION_FAILED."),
+    LOGIN_FAILED(HttpStatus.NOT_FOUND, "AUTH_002", " LOGIN_FAILED."),
+    REGISTER_FAILED(HttpStatus.FORBIDDEN, "AUTH_003", "REGISTER_FAILED"),
+    REQUEST_PARAMETER_BIND_FAILED(HttpStatus.BAD_REQUEST, "REQ_001", "PARAMETER_BIND_FAILED");
+
+    private final String code;
+    private final String message;
+    private final HttpStatus status;
+
+    ErrorCode(final HttpStatus status, final String code, final String message){
+        this.status = status;
+        this.message = message;
+        this.code = code;
+    }
+}
