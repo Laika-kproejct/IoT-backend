3d09346ebaed920ac41a9c5c542480bf3fb77bfd
suhongkim98 <kshz9815@naver.com>
2021-09-21 00:15:45 +0900

    토큰 발급 테스트코드 작성

---
+++ src/test/java/com/laika/IoT/provider/security/JwtAuthTokenProviderTests.java
@@ -0,0 +1,28 @@
+package com.laika.IoT.provider.security;
+
+import com.laika.IoT.core.security.role.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@SpringBootTest
+public class JwtAuthTokenProviderTests {
+    @Autowired
+    JwtAuthTokenProvider jwtAuthTokenProvider;
+
+    @DisplayName("jwt 토큰 발급 테스트")
+    @Test
+    void createTokenTest() {
+        //given
+        Date expiredDate = Date.from(LocalDateTime.now().plusMinutes(2).atZone(ZoneId.systemDefault()).toInstant()); // 토큰은 2분만 유지되도록 설정, 2분 후 refresh token
+        //when
+        JwtAuthToken accessToken = jwtAuthTokenProvider.createAuthToken("helloId", Role.ADMIN.getCode(), expiredDate);  //토큰 발급
+        //then
+        System.out.println(accessToken.getToken());
+    }
+}
