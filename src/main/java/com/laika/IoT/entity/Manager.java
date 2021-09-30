514e2bf047c014870fd39e305aa9669295cb12e9
suhongkim98 <kshz9815@naver.com>
2021-09-30 14:20:48 +0900

    매니저 엔티티, 레포지토리 생성

---
+++ src/main/java/com/laika/IoT/entity/Manager.java
@@ -0,0 +1,36 @@
+package com.laika.IoT.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Table(name="Manager")
+@Entity
+@Getter
+@NoArgsConstructor
+public class Manager {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="salt")
+    private String salt;
+
+    @Column(name="refreshToken")
+    private String refreshToken;
+
+    @Builder
+    public Manager(String email, String password, String salt) {
+        this.email = email;
+        this.password = password;
+        this.salt = salt;
+    }
+}
