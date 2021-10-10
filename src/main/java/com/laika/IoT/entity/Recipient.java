481fe98e798dad06db5058fd539ea9038364532a
suhongkim98 <kshz9815@naver.com>
2021-10-10 17:55:59 +0900

    관리대상자, 센서 엔티티 생성

---
+++ src/main/java/com/laika/IoT/entity/Recipient.java
@@ -0,0 +1,28 @@
+package com.laika.IoT.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+//관리 대상자
+@Table(name="Recipient")
+@Entity
+@Getter
+@NoArgsConstructor
+public class Recipient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(mappedBy = "recipient")
+    private List<IoTSensor> sensors = new ArrayList<IoTSensor>();
+
+    public void addSensor(IoTSensor sensor) {
+        this.sensors.add(sensor);
+    }
+
+}
