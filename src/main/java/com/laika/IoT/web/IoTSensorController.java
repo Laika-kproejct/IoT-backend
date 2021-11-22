342fbaeea8bccce6a52ca9c2ed4d949513a8198f
skfkgla <narahim.lee@gmail.com>
2021-11-22 16:01:12 +0900

    sensor register change method

--- src/main/java/com/laika/IoT/web/IoTSensorController.java
+++ src/main/java/com/laika/IoT/web/IoTSensorController.java
@@ -15,11 +15,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Map;
 import java.util.Optional;
